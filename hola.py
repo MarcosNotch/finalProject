5ed9a0361d5d4b5b9815c0367f189d369d0b9e90
Marcos <marcoscanette1@gmail.com>
2022-04-24 17:08:34 -0300

    data collection

--- hola.py
+++ hola.py
@@ -1,1 +1,13 @@
-git
+import requests
+import pandas as pd
+
+url  = "https://api.spacexdata.com/v4/launches/past"
+
+response  = requests.get(url)
+
+
+# Con esto mapeamos un json a un dataframe
+
+data = pd.json_normalize(response.json())
+
+data
